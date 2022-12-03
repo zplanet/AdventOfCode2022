bc12414e5f6784588f0cd513619b7ca527928062
Jong Hoon Kim <zplanet@hotmail.com>
2022-12-02 22:03:45 -0500

    Day01

---
+++ Day01/Program.fs
@@ -0,0 +1,29 @@
+﻿open System.IO
+
+let input = 
+    Path.Combine(Directory.GetCurrentDirectory(), "input.txt")
+    |> File.ReadLines
+    |> Seq.map (fun s -> if s.Length > 0 then Some((int)s) else None)
+
+let calcCalories data =
+    data
+    |> Seq.fold 
+        (fun (h::ts) v -> 
+            match v with 
+            | Some n -> (h + n)::ts 
+            | None -> (0::h::ts)) 
+        [0]
+
+// part 1
+input
+|> calcCalories
+|> List.max
+|> printfn "part 1: %A"
+
+// part 2
+input
+|> calcCalories
+|> List.sortDescending
+|> List.take 3
+|> List.sum
+|> printfn "part 2: %A"
