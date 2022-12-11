9b4e326d35d655389cd5996bb97937436b49882a
Jong Hoon Kim <zplanet@hotmail.com>
2022-12-11 09:33:51 -0500

    Day09 Part2

--- Day09/Program.fs
+++ Day09/Program.fs
@@ -6,8 +6,7 @@ let input =
     |> File.ReadLines
 
 type Direction = Left | Right | Up | Down
-let initialHeadPos = (0, 0)
-let initialTailPos = (0, 0)
+let initialPos = (0, 0)
 
 let (|LEFT|_|) str =
    let m = Regex.Match(str, "L ([0-9]+)")
@@ -27,53 +26,89 @@ let (|DOWN|_|) str =
 
 let updatePos pos map = Map.change pos (function | Some v -> Some (v + 1) | None -> Some 1) map
 
-let simulate moves =
+let simulate positions moves =
     let isAdjacent (hx, hy) (tx, ty) = (abs (tx - hx)) < 2 && (abs (ty - hy)) < 2
-    let moveLeft (x, y) = (x - 1, y)
-    let moveRight (x, y) = (x + 1, y)
-    let moveUp (x, y) = (x, y + 1)
-    let moveDown (x, y) = (x, y - 1)
-    let getAdjacentPos (x, y) = function
-        | Left -> (x + 1, y)
-        | Right -> (x - 1, y)
-        | Up -> (x, y - 1)
-        | Down -> (x, y + 1)
+    let step (x, y) = function
+        | Left -> (x - 1, y)
+        | Right -> (x + 1, y)
+        | Up -> (x, y + 1)
+        | Down -> (x, y - 1)
+    let getAdjacentPos = function
+        | (x1, y1), (x2, y2) when x1 = x2 && y1 = y2 -> (x2, y2)
 
-    let rec move n direction currHeadPos currTailPos m =
-        if n = 0
-        then (currHeadPos, currTailPos, m)
+        | (x1, y1), (x2, y2) when x1 = x2 && y1 = (y2 + 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when x1 = x2 && y1 = (y2 + 2) -> (x2, y1 - 1)
+        | (x1, y1), (x2, y2) when x1 = x2 && y1 = (y2 - 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when x1 = x2 && y1 = (y2 - 2) -> (x2, y1 + 1)
+
+        | (x1, y1), (x2, y2) when y1 = y2 && x1 = (x2 + 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when y1 = y2 && x1 = (x2 + 2) -> (x1 - 1, y2)
+        | (x1, y1), (x2, y2) when y1 = y2 && x1 = (x2 - 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when y1 = y2 && x1 = (x2 - 2) -> (x1 + 1, y2)        
+        
+        | (x1, y1), (x2, y2) when x1 = (x2 + 1) && y1 = (y2 + 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when x1 = (x2 + 1) && y1 = (y2 - 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 1) && y1 = (y2 + 1) -> (x2, y2)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 1) && y1 = (y2 - 1) -> (x2, y2)
+
+        | (x1, y1), (x2, y2) when x1 = (x2 + 1) && y1 = (y2 + 2) -> (x1, y1 - 1)
+        | (x1, y1), (x2, y2) when x1 = (x2 + 1) && y1 = (y2 - 2) -> (x1, y1 + 1)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 1) && y1 = (y2 + 2) -> (x1, y1 - 1)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 1) && y1 = (y2 - 2) -> (x1, y1 + 1)
+
+        | (x1, y1), (x2, y2) when x1 = (x2 + 2) && y1 = (y2 + 1) -> (x1 - 1, y1)
+        | (x1, y1), (x2, y2) when x1 = (x2 + 2) && y1 = (y2 - 1) -> (x1 - 1, y1)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 2) && y1 = (y2 + 1) -> (x1 + 1, y1)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 2) && y1 = (y2 - 1) -> (x1 + 1, y1)
+
+        | (x1, y1), (x2, y2) when x1 = (x2 + 2) && y1 = (y2 + 2) -> (x2 + 1, y2 + 1)
+        | (x1, y1), (x2, y2) when x1 = (x2 + 2) && y1 = (y2 - 2) -> (x2 + 1, y2 - 1)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 2) && y1 = (y2 + 2) -> (x2 - 1, y2 + 1)
+        | (x1, y1), (x2, y2) when x1 = (x2 - 2) && y1 = (y2 - 2) -> (x2 - 1, y2 - 1)
+    
+    let rec apply r ps direction = 
+        if List.isEmpty r
+        then
+            apply ((step (List.head ps) direction)::r) (List.tail ps) direction
         else
-            match direction with
-            | Left -> 
-                let headPos = moveLeft currHeadPos
-                let tailPos = if (isAdjacent headPos currTailPos) then currTailPos else getAdjacentPos headPos Left
-                move (n - 1) direction headPos tailPos (updatePos tailPos m)
-            | Right ->
-                let headPos = moveRight currHeadPos
-                let tailPos = if (isAdjacent headPos currTailPos) then currTailPos else getAdjacentPos headPos Right
-                move (n - 1) direction headPos tailPos (updatePos tailPos m)
-            | Up ->
-                let headPos = moveUp currHeadPos
-                let tailPos = if (isAdjacent headPos currTailPos) then currTailPos else getAdjacentPos headPos Up
-                move (n - 1) direction headPos tailPos (updatePos tailPos m)
-            | Down ->
-                let headPos = moveDown currHeadPos
-                let tailPos = if (isAdjacent headPos currTailPos) then currTailPos else getAdjacentPos headPos Down
-                move (n - 1) direction headPos tailPos (updatePos tailPos m)
+            if List.isEmpty ps
+            then r
+            else 
+                let pos1 = List.head r
+                let pos2 = List.head ps
+                apply ((getAdjacentPos (pos1, pos2))::r) (List.tail ps) direction
+
+    let rec move n direction positions m =
+        if n = 0
+        then (positions, m)
+        else
+            let npositions = 
+                match direction with
+                | Left -> apply [] positions Left
+                | Right -> apply [] positions Right
+                | Up -> apply [] positions Up
+                | Down -> apply [] positions Down
+            move (n - 1) direction (List.rev npositions) (updatePos (List.head npositions) m)
 
     moves
     |> Seq.fold
-        (fun (currHeadPos, currTailPos, m) line ->
+        (fun (positions, m) line ->
             match line with
-            | LEFT n -> move n Left currHeadPos currTailPos m
-            | RIGHT n -> move n Right currHeadPos currTailPos m
-            | UP n -> move n Up currHeadPos currTailPos m
-            | DOWN n -> move n Down currHeadPos currTailPos m)
-        (initialHeadPos, initialTailPos, (updatePos initialTailPos Map.empty))
-    |> (fun (_, _, m) -> m)
+            | LEFT n -> move n Left positions m
+            | RIGHT n -> move n Right positions m
+            | UP n -> move n Up positions m
+            | DOWN n -> move n Down positions m)
+        (positions, (updatePos initialPos Map.empty))
+    |> snd
 
 input
-|> simulate
+|> simulate [ for i in 1 .. 2 -> initialPos]
 |> Map.toList
 |> List.length
 |> printfn "Part1: %A"
+
+input
+|> simulate [ for i in 1 .. 10 -> initialPos]
+|> Map.toList
+|> List.length
+|> printfn "Part2: %A"
