cac4fead9068711ccc0e812fed3f7e415e240498
Jong Hoon Kim <zplanet@hotmail.com>
2022-12-13 13:25:40 -0500

    Day11 Part2

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -7,32 +7,35 @@ let input =
 
 type Monkey = {
     Id: int;
-    Items: int list;
-    CalcWorryLevel: int -> int;
-    Test: int -> int;
-    InspectionCount: int;
+    Items: uint64 list;
+    CalcWorryLevel: uint64 -> uint64;
+    Divisor: uint64;
+    Test: uint64 -> int;
+    InspectionCount: uint64;
 }
 
-let parseMonkeys makeCalcWorryLevel =
+let monkeys =
     let pattern = "Monkey ([0-9]+):[^S]+Starting items: ([0-9 ,]+)[^O]+Operation: new = old ([*+]) (old|[0-9]+)[^T]+Test: divisible by ([0-9]+)[^I]+If true: throw to monkey ([0-9]+)[^I]+If false: throw to monkey ([0-9]+)"
     let matches = Regex.Matches (input, pattern, RegexOptions.Multiline)
 
-    let test divisor trueVal falseVal dividend = if dividend % divisor = 0 then trueVal else falseVal
-    let makeTest divisor trueVal falseVal = test divisor trueVal falseVal
+    let makeTest divisor trueVal falseVal = 
+        (fun (dividend: uint64) -> if dividend % divisor = 0UL then trueVal else falseVal)
+
+    let makeCalcWorryLevel f fb = (fun a -> f a (fb a))
 
     let createMonkey (m: Match) =
         let monkeyId = int m.Groups[1].Value
-        let items = m.Groups[2].Value.Split(", ") |> Array.map int |> Array.toList
+        let items = m.Groups[2].Value.Split(", ") |> Array.map uint64 |> Array.toList
         let calcWorryLevel =
             match m.Groups[3].Value, m.Groups[4].Value with
             | operator, operand when operator = "+" && operand = "old" -> makeCalcWorryLevel (+) id
             | operator, operand when operator = "*" && operand = "old" -> makeCalcWorryLevel (*) id
-            | operator, operand when operator = "+" -> makeCalcWorryLevel (+) (fun _ -> int operand)
-            | _, operand -> makeCalcWorryLevel (*) (fun _ -> int operand)
-        let divisor = int m.Groups[5].Value
+            | operator, operand when operator = "+" -> makeCalcWorryLevel (+) (fun _ -> uint64 operand)
+            | _, operand -> makeCalcWorryLevel (*) (fun _ -> uint64 operand)
+        let divisor = uint64 m.Groups[5].Value
         let monkeyIdTrue = int m.Groups[6].Value
         let monkeyIdFalse = int m.Groups[7].Value
-        { Id = monkeyId; Items = items; CalcWorryLevel = calcWorryLevel; Test = (makeTest divisor monkeyIdTrue monkeyIdFalse); InspectionCount = 0 }
+        { Id = monkeyId; Items = items; CalcWorryLevel = calcWorryLevel; Divisor = divisor; Test = (makeTest divisor monkeyIdTrue monkeyIdFalse); InspectionCount = 0UL }
 
     matches
     |> Seq.map createMonkey
@@ -41,12 +44,13 @@ let parseMonkeys makeCalcWorryLevel =
             Map.add monkey.Id monkey map)
         Map.empty
 
-let inspect (map: Map<int, Monkey>) index =
+let inspect relieve (map: Map<int, Monkey>) index =
     let id = index % map.Count
     let monkey = Map.find id map
     let worryLevels =
         monkey.Items
         |> List.map monkey.CalcWorryLevel
+        |> List.map relieve
         |> List.map (fun level -> (monkey.Test level, level))
     worryLevels
     |> List.fold
@@ -61,20 +65,31 @@ let inspect (map: Map<int, Monkey>) index =
     |> Map.change
         id
         (function
-        | Some m -> Some { m with Items = []; InspectionCount = m.InspectionCount + (List.length worryLevels) }
+        | Some m -> Some { m with Items = []; InspectionCount = m.InspectionCount + ((uint64)(List.length worryLevels)) }
         | None -> None)
 
-let makeCalcWorryLevel f fb = (fun a -> (f a (fb a)) / 3)
-let makeCalcWorryLevel2 (f: uint64 -> uint64 -> uint64) (fb: uint64 -> uint64) = (fun a -> f a (fb a))
+let processRound isRidiculous (monkeys: Map<int, Monkey>) _ = 
+    let modulo = monkeys |> Map.toList |> List.map (fun (_, m) -> m.Divisor) |> List.fold (fun acc v -> acc * v) 1UL
+    let relieve =
+        if isRidiculous
+        then (fun x -> x % modulo)
+        else (fun x -> x / 3UL)
 
-let processRound (monkeys: Map<int, Monkey>) _ = 
     [0 .. (monkeys.Count - 1)]
-    |> List.fold inspect monkeys
+    |> List.fold (inspect relieve) monkeys 
 
 [1..20]
-|> List.fold processRound (parseMonkeys makeCalcWorryLevel)
+|> List.fold (processRound false) monkeys
 |> Map.toList
 |> List.sortByDescending (fun (_, m) -> m.InspectionCount)
 |> List.take 2
-|> List.fold (fun acc (_, m) -> acc * m.InspectionCount) 1
+|> List.fold (fun acc (_, m) -> acc * m.InspectionCount) 1UL
 |> printfn "Part1: %A"
+
+[1..10000]
+|> List.fold (processRound true) monkeys
+|> Map.toList
+|> List.sortByDescending (fun (_, m) -> m.InspectionCount)
+|> List.take 2
+|> List.fold (fun acc (_, m) -> acc * m.InspectionCount) 1UL
+|> printfn "Part2: %A"
